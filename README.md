09d3f6de91079ac7618c58774a09e9e70c374014
Nelson Gill <robotrogue@hotmail.com>
2019-05-27 11:00:14 -0700

    Let Prettier do its thing

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A dark slack theme for the desktop client
 ## Installation Instructions:
 You can find the install instructions from [this site](https://www.howtogeek.com/368976/how-to-install-the-unofficial-dark-mode-for-slack/)
 
-This theme is borrowed heavily from [this fork](https://github.com/caiceA/slack-black-theme) ... I will properly fork from the origin once I get all the styles configured the way I want them to look. 
+This theme is borrowed heavily from [this fork](https://github.com/caiceA/slack-black-theme) ... I will properly fork from the origin once I get all the styles configured the way I want them to look.
 
 Also added a bunch of style fixes that were missed by the forked version such as alignment and buttons/text that was still too difficult to read.
+
+An empty theme for your own customizations can be found [here](https://cdn.rawgit.com/widget-/slack-black-theme/master/custom.css)
--- ssb-interop.js
+++ ssb-interop.js
@@ -4,116 +4,118 @@
  */
 
 const allowedChildWindowEventMethod = [
-    "windowWithTokenBeganLoading",
-    "windowWithTokenFinishedLoading",
-    "windowWithTokenCrashed",
-    "windowWithTokenDidChangeGeometry",
-    "windowWithTokenBecameKey",
-    "windowWithTokenResignedKey",
-    "windowWithTokenWillClose"
+  "windowWithTokenBeganLoading",
+  "windowWithTokenFinishedLoading",
+  "windowWithTokenCrashed",
+  "windowWithTokenDidChangeGeometry",
+  "windowWithTokenBecameKey",
+  "windowWithTokenResignedKey",
+  "windowWithTokenWillClose"
 ];
 
 if (window.location.href !== "about:blank") {
-    require("./assign-metadata").assignMetadata();
-    const preloadStartTime = process.hrtime();
+  require("./assign-metadata").assignMetadata();
+  const preloadStartTime = process.hrtime();
 
-    if (window.parentWebContentsId) {
+  if (window.parentWebContentsId) {
     //tslint:disable-next-line:no-console max-line-length
-        const warn = () =>
-            console.warn(
-            `Deprecated: direct access to global object 'parentInfo' will be disallowed. 'parentWebContentsId' will be available until new interface is ready.`
-            );
-        Object.defineProperty(window, "parentInfo", {
-            get: () => {
+    const warn = () =>
+      console.warn(
+        `Deprecated: direct access to global object 'parentInfo' will be disallowed. 'parentWebContentsId' will be available until new interface is ready.`
+      );
+    Object.defineProperty(window, "parentInfo", {
+      get: () => {
+        warn();
+        return {
+          get webContentsId() {
             warn();
-                return {
-                    get webContentsId() {
-                    warn();
-                    return parentWebContentsId;
-                    }
-                };
-            }
-        });
-    }
+            return parentWebContentsId;
+          }
+        };
+      }
+    });
+  }
 
-    const { ipcRenderer, remote } = require("electron");
+  const { ipcRenderer, remote } = require("electron");
 
-    ipcRenderer.on(
+  ipcRenderer.on(
     "SLACK_NOTIFY_CHILD_WINDOW_EVENT",
     (event, method, ...args) => {
-        try {
-            if (!TSSSB || !TSSSB[method])
-                throw new Error("Webapp is not fully loaded to execute method");
-            if (!allowedChildWindowEventMethod.includes(method)) {
-                throw new Error("Unsupported method");
-            }
-        TSSSB[method](...args);
-        } catch (error) {
-            console.error(`Cannot execute method`, { error, method }); //tslint:disable-line:no-console
+      try {
+        if (!TSSSB || !TSSSB[method])
+          throw new Error("Webapp is not fully loaded to execute method");
+        if (!allowedChildWindowEventMethod.includes(method)) {
+          throw new Error("Unsupported method");
         }
-    });
+        TSSSB[method](...args);
+      } catch (error) {
+        console.error(`Cannot execute method`, { error, method }); //tslint:disable-line:no-console
+      }
+    }
+  );
 
-    ipcRenderer.on(
+  ipcRenderer.on(
     "SLACK_REMOTE_DISPATCH_EVENT",
     (event, data, origin, browserWindowId) => {
-        const evt = new Event("message");
-        evt.data = JSON.parse(data);
-        evt.origin = origin;
-        evt.source = {
-            postMessage: message => {
-                if (!desktop || !desktop.window || !desktop.window.postMessage)
-                throw new Error("desktop not ready");
-                return desktop.window.postMessage(message, browserWindowId);
-            }
-        };
-        window.dispatchEvent(evt);
-        event.sender.send("SLACK_REMOTE_DISPATCH_EVENT");
-    });
+      const evt = new Event("message");
+      evt.data = JSON.parse(data);
+      evt.origin = origin;
+      evt.source = {
+        postMessage: message => {
+          if (!desktop || !desktop.window || !desktop.window.postMessage)
+            throw new Error("desktop not ready");
+          return desktop.window.postMessage(message, browserWindowId);
+        }
+      };
+      window.dispatchEvent(evt);
+      event.sender.send("SLACK_REMOTE_DISPATCH_EVENT");
+    }
+  );
 
-    const { init } = require("electron-compile");
-    const { assignIn } = require("lodash");
-    const path = require("path");
-    const { isPrebuilt } = require("../utils/process-helpers");
+  const { init } = require("electron-compile");
+  const { assignIn } = require("lodash");
+  const path = require("path");
+  const { isPrebuilt } = require("../utils/process-helpers");
 
-    //tslint:disable-next-line:no-console
-    process.on("uncaughtException", e => console.error(e));
+  //tslint:disable-next-line:no-console
+  process.on("uncaughtException", e => console.error(e));
 
-    /**
-     * Patch Node.js globals back in, refer to
-     * https://electron.atom.io/docs/api/process/#event-loaded.
-     */
-    const processRef = window.process;
+  /**
+   * Patch Node.js globals back in, refer to
+   * https://electron.atom.io/docs/api/process/#event-loaded.
+   */
+  const processRef = window.process;
 
-    process.once("loaded", () => {
-        window.process = processRef;
-    });
+  process.once("loaded", () => {
+    window.process = processRef;
+  });
 
-    window.perfTimer.PRELOAD_STARTED = preloadStartTime;
+  window.perfTimer.PRELOAD_STARTED = preloadStartTime;
 
-    // Consider "initial team booted" as whether the workspace is the first loaded after Slack launches
-    ipcRenderer.once("SLACK_PRQ_TEAM_BOOT_ORDER", (_event, order) => {
-        window.perfTimer.isInitialTeamBooted = order === 1;
-    });
+  // Consider "initial team booted" as whether the workspace is the first loaded after Slack launches
+  ipcRenderer.once("SLACK_PRQ_TEAM_BOOT_ORDER", (_event, order) => {
+    window.perfTimer.isInitialTeamBooted = order === 1;
+  });
 
-    ipcRenderer.send("SLACK_PRQ_TEAM_BOOTED"); // Main process will respond SLACK_PRQ_TEAM_BOOT_ORDER
+  ipcRenderer.send("SLACK_PRQ_TEAM_BOOTED"); // Main process will respond SLACK_PRQ_TEAM_BOOT_ORDER
 
-    const resourcePath = path.join(__dirname, "..", "..");
-    const mainModule = require.resolve("../ssb/main.ts");
-    const isDevMode = loadSettings.devMode && isPrebuilt();
+  const resourcePath = path.join(__dirname, "..", "..");
+  const mainModule = require.resolve("../ssb/main.ts");
+  const isDevMode = loadSettings.devMode && isPrebuilt();
 
-    init(resourcePath, mainModule, !isDevMode);
+  init(resourcePath, mainModule, !isDevMode);
 }
 
 // Slack Night Mood theme
 document.addEventListener("DOMContentLoaded", function() {
-    // Then get its webviews
-    let webviews = document.querySelectorAll(".TeamView webview");
-    // Fetch our CSS in parallel ahead of time
-    const cssPath =
+  // Then get its webviews
+  let webviews = document.querySelectorAll(".TeamView webview");
+  // Fetch our CSS in parallel ahead of time
+  const cssPath =
     "https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css";
-    const localCssPath = "sib.css";
-    let cssPromise = fetch(cssPath).then(response => response.text());
-    let customCustomCSS = `
+  const localCssPath = "sib.css";
+  let cssPromise = fetch(cssPath).then(response => response.text());
+  let customCustomCSS = `
         :root {
         /* Modify these to change your theme colors: */
         --primary: #61AFEF;
@@ -1258,31 +1260,31 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     `;
 
-    // $("div").each(function(){
-    // 	    $this.css("color","white");
-    // });
-    // Insert a style tag into the wrapper view
-    cssPromise.then(css => {
-        let s = document.createElement("style");
-        s.type = "text/css";
-        s.innerHTML = css + customCustomCSS;
-        document.head.appendChild(s);
-    });
-    // Wait for each webview to load
-    webviews.forEach(webview => {
-        webview.addEventListener("ipc-message", message => {
-            if (message.channel == "didFinishLoading")
-            // Finally add the CSS into the webview
-            cssPromise.then(css => {
-                let script = `
+  // $("div").each(function(){
+  // 	    $this.css("color","white");
+  // });
+  // Insert a style tag into the wrapper view
+  cssPromise.then(css => {
+    let s = document.createElement("style");
+    s.type = "text/css";
+    s.innerHTML = css + customCustomCSS;
+    document.head.appendChild(s);
+  });
+  // Wait for each webview to load
+  webviews.forEach(webview => {
+    webview.addEventListener("ipc-message", message => {
+      if (message.channel == "didFinishLoading")
+        // Finally add the CSS into the webview
+        cssPromise.then(css => {
+          let script = `
                     let s = document.createElement('style');
                     s.type = 'text/css';
                     s.id = 'slack-custom-css';
                     s.innerHTML = \`${css + customCustomCSS}\`;
                     document.head.appendChild(s);
                 `;
-                webview.executeJavaScript(script);
-            });
+          webview.executeJavaScript(script);
         });
     });
+  });
 });
